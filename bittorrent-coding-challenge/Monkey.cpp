3413bac2ca7036cef8699a1637b06a9c4f76df15
moneyman101k <moneyman.karipineni@gmail.com>
2016-02-28 16:11:54 -0800

    Converted solution to use a global set instead of passing set by reference, updated README.txt as well

--- bittorrent-coding-challenge/Monkey.cpp
+++ bittorrent-coding-challenge/Monkey.cpp
@@ -5,6 +5,53 @@
 using namespace std;
 
 
+// Set and Function Declarations (Set to Be Used Globally)
+set< pair<int, int> > coords;
+int monkeyCoordinate(int x, int y);
+int absoluteDigitSumCoordinate(int x, int y);
+
+
+// Main Function to Make Inital Call to Recursive Function
+int main()
+{
+	
+	// Function Returns Number of Valid Monkey Coordinates
+	int count = monkeyCoordinate(0, 0);
+
+	// STDOUT Output for the Answer
+	cout << "Number of Accessible Monkey Coordinates = " << count << endl;
+
+	return 0;
+}
+
+
+// Recursive Function to Determine if Current Monkey Coordinate is Valid or not
+// Makes Recursive Call on 4 Adjacent Coordinates if a New, Valid Coordinate
+int monkeyCoordinate(int x, int y){
+
+	// Check the Sum to See if it Represents a Valid Coordinate
+	if(absoluteDigitSumCoordinate(x, y) <= 19){
+
+		// Check to See if the Valid Coordinate is Already in the Set, By Checking on the Coordinate Pair
+		if(coords.find(make_pair(x, y)) != coords.end()){
+			return 0;
+		}
+		
+		// If the Valid Coordinate is not Already in the Set, Add it to the Set
+		coords.insert(make_pair(x, y));
+
+		// Add 1 to the Number of Valid Coordinates, and Make Recursive Calls on the 4 Adjacent Points
+		return 1 + monkeyCoordinate(x-1, y) + monkeyCoordinate(x+1, y)
+				 + monkeyCoordinate(x, y-1) + monkeyCoordinate(x, y+1);
+	}
+
+	// If the Coordinate is Invalid, Don't Do Anything with it
+	return 0;
+
+}
+
+
+// Function to Compute the Sum of the Digits of Coordinates in Absolute Value
 int absoluteDigitSumCoordinate(int x, int y){
 
 	// Variable for the Sum of Digits for Current Coordinate Pair
@@ -30,46 +77,8 @@ int absoluteDigitSumCoordinate(int x, int y){
 		check_y /= 10;
 	}
 
-	// Return the Integer Sum
+	// Return the Integer Sum (to be used by Recursive Function)
 	return sum;
 
 }
 
-
-int monkeyCoordinate(int x, int y, set<pair <int, int> > &coords){
-
-	// Check the Sum to See if it Represents a Valid Coordinate
-	if(absoluteDigitSumCoordinate(x, y) <= 19){
-
-		// Check to See if the Valid Coordinate is Already in the Set, By Checking on the Coordinate Pair
-		if(coords.find(make_pair(x, y)) != coords.end()){
-			return 0;
-		}
-		
-		// If the Valid Coordinate is not Already in the Set, Add it to the Set
-		coords.insert(make_pair(x, y));
-
-		// Add 1 to the Number of Valid Coordinates, and Make Recursive Calls on the 4 Adjacent Points
-		return 1 + monkeyCoordinate(x-1, y, coords) + monkeyCoordinate(x+1, y, coords)
-				 + monkeyCoordinate(x, y-1, coords) + monkeyCoordinate(x, y+1, coords);
-	}
-
-	// If the Coordinate is Invalid, Don't Do Anything with it
-	return 0;
-
-}
-
-
-int main()
-{
-	// Set Used to Fill in with Coordinates
-	set< pair<int, int> > coords;
-
-	// Pass the Coordinate Set into the Function - Accessed within Function by Reference
-	monkeyCoordinate(0, 0, coords);
-
-	// STDOUT Output for the Answer
-	cout << "Total Valid Monkey Coordinates = " << coords.size() << endl;
-
-	return 0;
-}
